a7b505019240dc36e78426bf87b3a01798ba200e
BlakiDarkness <blakibang@gmail.com>
2020-03-25 19:56:51 -0600

    Validación

--- db/migrate/20200325001536_create_categories.rb
+++ db/migrate/20200325001536_create_categories.rb
@@ -1,7 +1,6 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.integer :id
       t.string  :name, null: false
       t.string  :description
     end
